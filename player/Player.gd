3e8b40965cbb64e5b00e91d469cb879021acc2bf
Alexis Schad <schad.alexis@gmail.com>
2021-04-25 01:28:16 +0200

    run animation

--- player/Player.gd
+++ player/Player.gd
@@ -1,14 +1,16 @@
 extends KinematicBody
 
-# How fast the player moves in meters per second.
 export var speed = 7
-# The downward acceleration when in the air, in meters per second squared.
 export var gravity = 75
+export var acceleration = 15
 
 var velocity = Vector3.ZERO
 
 onready var camera = $Camera
 onready var pivot = $Pivot
+onready var animationTree = $Pivot/Mike/AnimationTree
+
+const RUN_PARAM = "parameters/run_blend/blend_amount"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,6 +20,7 @@ func _ready():
 func _physics_process(delta):
 	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
+	var da = delta * acceleration
 
 	# We check for each move input and update the direction accordingly.
 	direction.x += Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
@@ -29,11 +32,16 @@ func _physics_process(delta):
 			direction = direction.normalized()
 		pivot.look_at(translation + direction, Vector3.UP)
 	
+	animationTree.set(RUN_PARAM, lerp(animationTree.get(RUN_PARAM), direction.length(), da))
+	
 	# Ground velocity
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
+	velocity.x = lerp(velocity.x, direction.x * speed, da)
+	velocity.z = lerp(velocity.z, direction.z * speed, da)
 	# Vertical velocity
 	velocity.y -= gravity * delta
 	# Moving the character
 	velocity = move_and_slide(velocity, Vector3.UP)
-	
+
+
+func _on_Area_body_entered(body):
+	print_debug(body.get_node("../").name)
