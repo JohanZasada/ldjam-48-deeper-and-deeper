4321b95dd057b38f35abbd13fa288dafd69a3bd4
JohanZasada <zasada.johan@gmail.com>
2021-04-26 14:21:33 +0200

    added: start menu

--- hud/Hud.gd
+++ hud/Hud.gd
@@ -2,9 +2,15 @@ extends Control
 
 
 func _ready():
-	pass
+	$Popup2.popup()
 
 
 func _on_Panel_gui_input(event):
 	if event is InputEventMouseButton:
 		get_tree().reload_current_scene()
+
+
+func _on_StartPanet_gui_input(event):
+	if event is InputEventMouseButton:
+		get_tree().paused = false
+		$Popup2.hide()
